e8491d397b6e6f43ce23741d1e30b5b1c7c2a233
bendo <zdeno.osina@gmail.com>
2016-11-06 00:11:57 +0100

    button demo - playing with circle

--- arduboy/buttons/src/main.ino
+++ arduboy/buttons/src/main.ino
@@ -2,29 +2,58 @@
 
 Arduboy arduboy;
 
+int16_t x, y;
+uint8_t r;
+
 void setup() {
-    arduboy.begin();
+    arduboy.beginNoLogo();
     arduboy.setFrameRate(15);
+    centerCircle();
 }
 
 void loop() {
     if (!(arduboy.nextFrame()))
         return;
 
-    if (arduboy.pressed(A_BUTTON)) {
-        clearDisplay();
-        arduboy.print("Hello!");    
+    if (arduboy.pressed(A_BUTTON) && (r > 0)) {
+        r--;
     }
 
-    if (arduboy.pressed(B_BUTTON)) {
-        clearDisplay();
-        arduboy.print("World!");    
+    if (arduboy.pressed(B_BUTTON) && fitIn()) {
+        r++;
     }
 
+    if (arduboy.pressed(LEFT_BUTTON) && (x > r)) {
+        x--;
+    }
+
+    if (arduboy.pressed(RIGHT_BUTTON) && (x + r < WIDTH - 1)) {
+        x++;
+    }
+
+    if (arduboy.pressed(UP_BUTTON) && (y > r)) {
+        y--;
+    }
+
+    if (arduboy.pressed(DOWN_BUTTON) && (y + r < HEIGHT - 1)) {
+        y++;
+    }
+
+    if (arduboy.pressed(A_BUTTON + B_BUTTON)) {
+        centerCircle();
+    }
+
+    arduboy.clear();
+    arduboy.drawCircle(x, y, r, 1);
     arduboy.display();
 }
 
-void clearDisplay() {
-    arduboy.clear();
-    arduboy.setCursor(0, 0);
+void centerCircle() {
+    x = WIDTH / 2;
+    y = HEIGHT / 2;
+    r = 5;
+}
+
+boolean fitIn() {
+    return (x > r) && (y > r) && (x + r < WIDTH - 1) && (y + r < HEIGHT - 1);
 }
