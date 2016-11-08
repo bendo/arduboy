4b1ec721207f07eff0a2c4a9599394b9ce63e340
bendo <zdeno.osina@gmail.com>
2016-11-08 09:20:18 +0100

    converting to decimal

--- arduboy/bin2dec/src/bin2dec.ino
+++ arduboy/bin2dec/src/bin2dec.ino
@@ -10,7 +10,13 @@ char strOut[6]{"00000"};
 
 void printLogo();
 
+void bin2dec();
+
+void dec_to_str (char* str, uint32_t val, size_t digits);
+
 void updateDisplay();
+
+uint32_t my_pow(uint32_t x, uint32_t n);
 
 void setup() {
     arduboy.beginNoLogo();
@@ -35,8 +41,8 @@ void loop() {
 
     if (arduboy.pressed(UP_BUTTON) || arduboy.pressed(DOWN_BUTTON)) {
         strIn[position] ^= 0x01;
-        updateDisplay();
         bin2dec();
+        updateDisplay();
     }
 
     delay(100);
@@ -51,7 +57,23 @@ void printLogo() {
 }
 
 void bin2dec() {
-    // convert to dec
+    int len = sizeof strIn / sizeof *strIn;
+    int d, j = 0;
+    for (int i = len-2; i >= 0; i--) {
+        d += (strIn[i] - '0') * my_pow(2, j);
+        j++;
+    }
+    dec_to_str(strOut, d, 5);
+}
+
+void dec_to_str (char* str, uint32_t val, size_t digits) {
+    size_t i=1u;
+
+    for(; i <= digits; i++) {
+        str[digits-i] = (char)((val % 10u) + '0');
+        val /= 10u;
+    }
+    str[i-1u] = '\0';
 }
 
 void updateDisplay() {
@@ -63,4 +85,16 @@ void updateDisplay() {
     arduboy.setCursor(80, 32);
     arduboy.print(strOut);
     arduboy.display();
+}
+
+uint32_t my_pow(uint32_t x, uint32_t n) {
+    uint32_t r = 1;
+    if(n == 0)
+        return 1;
+
+    while (n != 0) {
+        r *= x;
+        n--;
+    }
+    return r;
 }
