7d02b38a0525b6faf60a810391233325e6d5efa4
bendo <zdeno.osina@gmail.com>
2017-03-14 03:13:21 +0100

    Clean pong.ino code

--- games/pong/src/pong.ino
+++ games/pong/src/pong.ino
@@ -4,117 +4,125 @@ Arduboy arduboy;
 
 int state = 0;
 int pressed = 0;
-int ballx = 62;
-int bally = 0;
-int ballSize = 4;
+int ball_x = 62;
+int ball_y = 0;
+int ball_size = 4;
 int right = 1;
 int down = 1;
-int paddleWidth = 4;
-int paddleHeight = 9;
-int playerx = 0;
-int playery = 0;
-int computerx = 127 - paddleWidth;
-int computery = 0;
-int playerScore = 0;
-int computerScore = 0;
+int paddle_width = 4;
+int paddle_height = 9;
+int player_x = 0;
+int player_y = 0;
+int computer_x = 127 - paddle_width;
+int computer_y = 0;
+int player_score = 0;
+int computer_score = 0;
 
-void setup() {
-    arduboy.begin();
-    srand(7/8);
-    arduboy.setFrameRate(60);
-    arduboy.clear();
+void setup()
+{
+	arduboy.begin();
+	srand(7/8);
+	arduboy.setFrameRate(60);
+	arduboy.clear();
 }
 
-void loop() {
-    if (!arduboy.nextFrame()) {
-	return;
-    }
+void loop()
+{
+	if (!arduboy.nextFrame())
+		return;
 
-    arduboy.clear();
-    arduboy.setCursor(0, 0);
+	arduboy.clear();
+	arduboy.setCursor(0, 0);
 
-    switch (state) {
-        case 0:
-            arduboy.print("title");
-            break;
-        case 1:
-            printScore();
-            play();
-            countScore();
-            break;
-        case 2:
-            arduboy.print("win");
-            resetScore();
-            break;
-        case 3:
-            arduboy.print("lose");
-            resetScore();
-            break;
-    }
-    moveToScreen();
-    arduboy.display();
+	switch (state) {
+		case 0:
+			arduboy.print("title");
+			break;
+		case 1:
+			print_score();
+			play();
+			count_score();
+			break;
+		case 2:
+			arduboy.print("win");
+			reset_score();
+			break;
+		case 3:
+			arduboy.print("lose");
+			reset_score();
+			break;
+	}
+	move_to_screen();
+	arduboy.display();
 }
 
-void printScore() {
-    arduboy.setCursor(20, 0);
-    arduboy.print(playerScore);
-    arduboy.setCursor(101, 0);
-    arduboy.print(computerScore);
+void print_score()
+{
+	arduboy.setCursor(20, 0);
+	arduboy.print(player_score);
+	arduboy.setCursor(101, 0);
+	arduboy.print(computer_score);
 }
 
-void play() {
-    // move the ball
-    arduboy.fillRect(ballx, bally, ballSize, ballSize, WHITE);
-    right == 1 ? ballx++ : ballx--;
-    boolean playerHit = ballx == playerx + paddleWidth && playery < bally +
-    ballSize && playery + paddleHeight > bally;
-    boolean computerHit = ballx + ballSize == computerx && computery < bally +
-    ballSize && computery + paddleHeight > bally;
-    right = playerHit ? 1 : computerHit ? 0 : right;
-    down == 1 ? bally++ : bally--;
-    down = bally == 0 ? 1 : bally + ballSize == 63 ? 0 : down;
-    // move player's paddle
-    arduboy.fillRect(playerx, playery, paddleWidth, paddleHeight, WHITE);
-    if (arduboy.pressed(UP_BUTTON) && playery > 0) {
-        playery--;
-    }
-    if (arduboy.pressed(DOWN_BUTTON) && playery + paddleHeight < 63) {
-        playery++;
-    }
-    // move computer's paddle
-    arduboy.fillRect(computerx, computery, paddleWidth, paddleHeight, WHITE);
-    if (ballx > 110 || rand() % 20 == 1) {
-        bally < computery ? computery-- : bally + ballSize > computery +
-        paddleHeight ? computery++ : computery = computery;
-    }
+void play()
+{
+	// move the ball
+	arduboy.fillRect(ball_x, ball_y, ball_size, ball_size, WHITE);
+	right == 1 ? ball_x++ : ball_x--;
+	boolean playerHit = ball_x == player_x + paddle_width && player_y < ball_y +
+	ball_size && player_y + paddle_height > ball_y;
+	boolean computerHit = ball_x + ball_size == computer_x && computer_y < ball_y +
+	ball_size && computer_y + paddle_height > ball_y;
+	right = playerHit ? 1 : computerHit ? 0 : right;
+	down == 1 ? ball_y++ : ball_y--;
+	down = ball_y == 0 ? 1 : ball_y + ball_size == 63 ? 0 : down;
+
+	// move player's paddle
+	arduboy.fillRect(player_x, player_y, paddle_width, paddle_height, WHITE);
+	if (arduboy.pressed(UP_BUTTON) && player_y > 0) {
+		player_y--;
+	}
+	if (arduboy.pressed(DOWN_BUTTON) && player_y + paddle_height < 63) {
+		player_y++;
+	}
+
+	// move computer's paddle
+	arduboy.fillRect(computer_x, computer_y, paddle_width, paddle_height, WHITE);
+	if (ball_x > 110 || rand() % 20 == 1) {
+		ball_y < computer_y ? computer_y-- : ball_y + ball_size > computer_y +
+		paddle_height ? computer_y++ : computer_y = computer_y;
+	}
 }
 
-void countScore() {
-    if (ballx < - 10) {
-        ballx = 64;
-        right = 1;
-        computerScore++;
-    }
-    if (ballx > 138) {
-        ballx = 64;
-        right = 0;
-        playerScore++;
-    }
-    state = playerScore == 5 ? 2 : computerScore == 5 ? 3 : state;
+void count_score()
+{
+	if (ball_x < -10) {
+		ball_x = 64;
+		right = 1;
+		computer_score++;
+	}
+	if (ball_x > 138) {
+		ball_x = 64;
+		right = 0;
+		player_score++;
+	}
+	state = player_score == 5 ? 2 : computer_score == 5 ? 3 : state;
 }
 
-void resetScore() {
-    playerScore = 0;
-    computerScore = 0;
-    ballx = 64;
+void reset_score()
+{
+	player_score = 0;
+	computer_score = 0;
+	ball_x = 64;
 }
 
-void moveToScreen() {
-    if (arduboy.pressed(A_BUTTON) && pressed == 0) {
-        state = state < 3 ? state + 1 : 0;
-        pressed = 1;
-    }
-    if (arduboy.notPressed(A_BUTTON)) {
-        pressed = 0;
-    }
+void move_to_screen()
+{
+	if (arduboy.pressed(A_BUTTON) && pressed == 0) {
+		state = state < 3 ? state + 1 : 0;
+		pressed = 1;
+	}
+	if (arduboy.notPressed(A_BUTTON)) {
+		pressed = 0;
+	}
 }
