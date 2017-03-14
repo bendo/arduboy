8ef39a138506be763835c9d05b227c4082197381
bendo <zdeno.osina@gmail.com>
2017-03-14 03:11:55 +0100

    Clean buttons.ino code

--- demos/buttons/src/buttons.ino
+++ demos/buttons/src/buttons.ino
@@ -5,59 +5,49 @@ Arduboy arduboy;
 int16_t x, y;
 uint8_t r;
 
-void centerCircle();
+void center_circle();
 
-boolean fitIn(); 
+int fit_in();
 
-void setup() {
-    arduboy.beginNoLogo();
-    arduboy.setFrameRate(15);
-    centerCircle();
+void setup()
+{
+	arduboy.beginNoLogo();
+	arduboy.setFrameRate(15);
+	center_circle();
 }
 
-void loop() {
-    if (!(arduboy.nextFrame()))
-        return;
+void loop()
+{
+	if (!(arduboy.nextFrame()))
+		return;
+	if (arduboy.pressed(A_BUTTON) && (r > 0))
+		r--;
+	if (arduboy.pressed(B_BUTTON) && fit_in())
+		r++;
+	if (arduboy.pressed(LEFT_BUTTON) && (x > r))
+		x--;
+	if (arduboy.pressed(RIGHT_BUTTON) && (x + r < WIDTH - 1))
+		x++;
+	if (arduboy.pressed(UP_BUTTON) && (y > r))
+		y--;
+	if (arduboy.pressed(DOWN_BUTTON) && (y + r < HEIGHT - 1))
+		y++;
+	if (arduboy.pressed(A_BUTTON + B_BUTTON))
+		center_circle();
 
-    if (arduboy.pressed(A_BUTTON) && (r > 0)) {
-        r--;
-    }
-
-    if (arduboy.pressed(B_BUTTON) && fitIn()) {
-        r++;
-    }
-
-    if (arduboy.pressed(LEFT_BUTTON) && (x > r)) {
-        x--;
-    }
-
-    if (arduboy.pressed(RIGHT_BUTTON) && (x + r < WIDTH - 1)) {
-        x++;
-    }
-
-    if (arduboy.pressed(UP_BUTTON) && (y > r)) {
-        y--;
-    }
-
-    if (arduboy.pressed(DOWN_BUTTON) && (y + r < HEIGHT - 1)) {
-        y++;
-    }
-
-    if (arduboy.pressed(A_BUTTON + B_BUTTON)) {
-        centerCircle();
-    }
-
-    arduboy.clear();
-    arduboy.drawCircle(x, y, r, 1);
-    arduboy.display();
+	arduboy.clear();
+	arduboy.drawCircle(x, y, r, 1);
+	arduboy.display();
 }
 
-void centerCircle() {
-    x = WIDTH / 2;
-    y = HEIGHT / 2;
-    r = 5;
+void center_circle()
+{
+	x = WIDTH / 2;
+	y = HEIGHT / 2;
+	r = 5;
 }
 
-boolean fitIn() {
-    return (x > r) && (y > r) && (x + r < WIDTH - 1) && (y + r < HEIGHT - 1);
+int fit_in()
+{
+	return (x > r) && (y > r) && (x + r < WIDTH - 1) && (y + r < HEIGHT - 1);
 }
